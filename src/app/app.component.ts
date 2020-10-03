c77bc3e243cd0ac7e5d88bffe58ea9c3d8c4f51c
lkre <lukasz.res@gmail.com>
2020-10-03 15:15:19 +0200

    zad 10

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,24 @@
 import {Component, OnInit} from '@angular/core';
+import {MoviesServiceService} from './movies-service.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [MoviesServiceService]
 })
 export class AppComponent implements OnInit {
-  isDark = false;
+  private service: MoviesServiceService;
+  topMovies: any;
+  bottomMovies: any;
+
+  constructor(service: MoviesServiceService) {
+    this.service = service;
+  }
 
   ngOnInit(): void {
+    this.topMovies = this.service.getTopMovies();
+    this.bottomMovies = this.service.getBottomMovies();
   }
 
-  changeStyle(): void {
-    this.isDark = !this.isDark;
-  }
 }
