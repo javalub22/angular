0578ad4906830af41209254d2cd3cbbf152483c3
lkre <lukasz.res@gmail.com>
2020-10-03 13:53:10 +0200

    zad 6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,26 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  tab = 0;
+export class AppComponent implements OnInit {
+  borderStyle = '1px solid black';
 
-  setTab(num: number): void {
-    this.tab = num;
+  alertStyle = {
+    color: 'red',
+    fontWeight: 'bold',
+    borderBottom: this.borderStyle
+  };
+
+  style: any;
+
+  ngOnInit(): void {
+    this.setStyle(this.alertStyle);
   }
 
-  isSelected(num: number): boolean {
-    return this.tab === num;
+  setStyle(style: any) {
+    this.style = style;
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { MyComponentComponent } from './my-component/my-component.component';
-import { TabComponentComponent } from './tab-component/tab-component.component';
-import { TabContentComponentComponent } from './tab-content-component/tab-content-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MyComponentComponent,
-    TabComponentComponent,
-    TabContentComponentComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
