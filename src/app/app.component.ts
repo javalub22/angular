57e2fcf1844bd941aba6e44cc8f37955cea43c04
lkre <lukasz.res@gmail.com>
2020-10-03 11:22:52 +0200

    zad 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-project';
+  exists = false;
+
+  toggleExists(): void {
+    this.exists = !this.exists;
+  }
 }
